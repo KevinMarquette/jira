c3fac05fd0a6cf37240ee10018d7a5e7209e1e27
Kevin Marquette <kmarquette@loandepot.com>
2019-03-16 03:06:55 -0700

    add required modules to build

--- build.ps1
+++ build.ps1
@@ -8,7 +8,9 @@ $Script:Modules = @(
     'Pester',
     'platyPS',
     'PSScriptAnalyzer',
-    'DependsOn'
+    'DependsOn',
+    'PSFramework',
+    'CredentialManager'
 )
 
 $Script:ModuleInstallScope = 'CurrentUser'
