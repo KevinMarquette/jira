663b249ab28c0d19d4f67b8807350d0537bca9c8
Kevin Marquette <kmarquette@loandepot.com>
2019-03-16 03:42:59 -0700

    Create unique local build version

--- BuildTasks/PublishVersion.Task.ps1
+++ BuildTasks/PublishVersion.Task.ps1
@@ -1,6 +1,6 @@
 task PublishVersion {
     [version] $sourceVersion = (Get-Metadata -Path $manifestPath -PropertyName 'ModuleVersion')
-    "##vso[build.updatebuildnumber]$sourceVersion"
+    "##vso[build.updatebuildnumber]$sourceVersion.$env:Build_BuildID"
 
     # Do the same for appveyor
     # https://www.appveyor.com/docs/build-worker-api/#update-build-details
