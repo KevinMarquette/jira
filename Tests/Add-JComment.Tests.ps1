0cbd80b7736e6ba4799faeba567951156729e32e
Kevin Marquette <kmarquette@loandepot.com>
2019-04-18 03:46:06 -0700

    Add proper async to add comment

--- Tests/Add-JComment.Tests.ps1
+++ Tests/Add-JComment.Tests.ps1
@@ -8,15 +8,15 @@ Describe "function Add-JComment" -Tag Integration {
         Add-JComment -ID $ticket -Comment 'Test Comment 1'
     }
 
-    It "Add comment to missing ticket should throw" {
-        {
-            Add-JComment -ID 'MISSING-01' -Comment 'Test Comment 1'
-        } | Should -Throw -ExceptionType ([JiraModule.JiraInvalidActionException])
-    }
-
     It "Add comment by issue" {
         $issue = Get-JIssue -ID $Ticket
         $issue | Should -Not -BeNullOrEmpty -Because 'We need a valid issue to test with'
         $issue | Add-JComment -Comment "Test Comment 3"
+    }
+
+    It "Add comment to missing ticket should throw" {
+        {
+            Add-JComment -ID 'MISSING-01' -Comment 'Test Comment 1' -ErrorAction Stop
+        } | Should -Throw -ExceptionType ([JiraModule.JiraInvalidActionException])
     }
 }
